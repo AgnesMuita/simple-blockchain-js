d75265f10f42a836b8ce38453fc226631bbf0cf0
Agnes Muita <agnes.muita@alternateltd.com>
2022-01-31 22:29:50 +0300

    add proof of work

--- main.js
+++ main.js
@@ -6,15 +6,27 @@ class Block{
     this.previousHash = previousHash;
     this.timestamp=timestamp; 
     this.hash = this.calculateHash();
+    this.nonce = 0; //relevant to the mineBlock function by making changes to the hash
   }
   calculateHash(){
-      return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+      return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)+ this.nonce ).toString();
+  }
+  mineBlock(difficulty){
+    //make the hash of our block begin with a certain number of zeros upto a certain set value of difficulty.The while loop runs
+    //until the hash has enough zeros
+    while(this.hash.substring(0, difficulty) !== Array(difficulty+1).join("0")){
+        this.nonce++;
+    //calculate the hash of this block
+        this.hash = this.calculateHash()
+    }
+    console.log("Block mined:" + this.hash)
   }
 }
 
 class BlockChain {
     constructor(){
       this.chain=[this.createGenesisBlock];//genesis block
+      this.difficulty =  2;
 
     }
     createGenesisBlock(){
@@ -25,13 +37,14 @@ class BlockChain {
     }
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash;//get the latest block and its hash
-        newBlock.hash = newBlock.calculateHash();//update the calculateHash function
+        // newBlock.hash = newBlock.calculateHash();//update the calculateHash function
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);//add new block to the blockchain
     }
     isChainValid(){
       for(let i = 1;i < this.chain.length; i++){
-        const currentBlock = this.chain(i);
-        const previousBlock = this.chain(i-1);
+        const currentBlock = this.chain[i];
+        const previousBlock = this.chain[i-1];
 
         if (currentBlock.hash !== currentBlock.calculateHash()){
             return false;
@@ -45,9 +58,22 @@ class BlockChain {
 }
 //testing by creating an instance of the blockchain
 let NjokiCoin = new BlockChain()
+
+console.log("Mining Block 1...")
 NjokiCoin.addBlock(new Block(1, "31/1/2022",{amount:4}));
+console.log("Mining Block 2...")
 NjokiCoin.addBlock(new Block(2, "2/2/2022",{amount:10}));
+console.log("Mining Block 2...")
 NjokiCoin.addBlock(new Block(1, "4/2/2022",{amount:20}));
 
+console.log('Is Blockchain valid?' + NjokiCoin.isChainValid())
+//break chain by changing amount
+NjokiCoin.chain[1].data={amount:100};
+//break the chain by recalculating hash
+NjokiCoin.chain[1].hash= NjokiCoin.chain[1].calculateHash();
+
+console.log('Is Blockchain valid?' + NjokiCoin.isChainValid())
 
 console.log(JSON.stringify(NjokiCoin, null,4 ));
+
+ 
